f852ea7316a3d6ad36c483a297c9664ab18ee93d
KrlN06 <krl.nalepa@yahoo.com>
2026-03-14 20:34:11 +0100

    Class definition sceleton

--- DynamicArray.cpp
+++ DynamicArray.cpp
@@ -1,3 +1,53 @@
 //
 // Created by KN on 14.03.2026.
 //
+#include "DynamicArray.h"
+#include <iostream>
+
+DynamicArray::DynamicArray() {
+    capacity = 1;
+    size = 0;
+    data = new int[capacity];
+}
+
+DynamicArray::~DynamicArray() {
+    delete [] data;
+}
+
+void insert(int index, int value) {
+    std::cout<<"DynamicArray::insert()"<<std::endl;
+}
+
+void push_front(int value) {
+    std::cout<<"DynamicArray::push_front()"<<std::endl;
+}
+void push_back(int value) {
+    std::cout<<"DynamicArray::push_back()"<<std::endl;
+}
+void remove(int index) {
+    std::cout<<"DynamicArray::remove()"<<std::endl;
+}
+void remove_front() {
+    std::cout<<"DynamicArray::remove_front()"<<std::endl;
+}
+void remove_back() {
+    std::cout<<"DynamicArray::remove_back()"<<std::endl;
+}
+
+int find(int value) {
+    std::cout<<"DynamicArray::find()"<<std::endl;
+    return 1;
+}
+
+int getSize() {
+    std::cout<<"DynamicArray::getSize()"<<std::endl;
+    return size;
+}
+bool isEmpty() {
+    std::cout<<"DynamicArray::isEmpty()"<<std::endl;
+    return size == 0;
+}
+
+void DynamicArray::print() {
+    std::cout<<"DynamicArray print()"<<std::endl;
+}
