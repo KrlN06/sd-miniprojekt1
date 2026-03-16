638256d87bf4d48c0a136d793c53a98adb3e8f8a
KrlN06 <krl.nalepa@yahoo.com>
2026-03-16 22:13:57 +0100

    remove uptade

--- main.cpp
+++ main.cpp
@@ -7,8 +7,15 @@ int main()
     array.push_back(11);
     array.push_back(12);
     array.push_back(13);
+    array.insert(1,0);
+    array.insert(12,1);
 
+
+    array.print();
+    array.remove(1);
     array.print();
 
     return 0;
 }
+
+
